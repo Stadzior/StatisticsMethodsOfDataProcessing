6382790134df1fb816aca086937ffd56bdea8e13
Stadzior <Stadzior@gmail.com>
2017-10-18 16:44:11 +0200

    UI boosted up.

--- StatisticsMethodsOfDataProcessing/StatisticsMethodsOfDataProcessing/MainWindow.xaml.cs
+++ StatisticsMethodsOfDataProcessing/StatisticsMethodsOfDataProcessing/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,15 @@ namespace StatisticsMethodsOfDataProcessing
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+            }
         }
     }
 }
